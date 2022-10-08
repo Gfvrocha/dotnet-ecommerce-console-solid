9386ec58b314b58814a32427c945e4b799caf004
Felipe Rocha <felipe.viega.r@gmail.com>
2022-10-08 18:29:21 -0300

    codigo sem principios

--- Program.cs
+++ Program.cs
@@ -6,12 +6,38 @@
         {
             new Cliente()
             {
+                Id = 1,
+                Nome = "Felipe",
+                Email = "felipe@teste.com",
+                EnderecoCompleto = "Rua teste 123 SP"
+            }.Salvar(Tipo.CSV);
+
+            new Cliente()
+            {
                 Id = 2,
                 Nome = "Gustavo",
                 Email = "gustavo@teste.com",
                 EnderecoCompleto = "Rua teste 123 SP"
-            }.Salvar();
-            Console.WriteLine("O cliente foi salvo com sucesso!");
+            }.Salvar(Tipo.CSV);
+
+            //Console.WriteLine("O cliente foi salvo com sucesso!");
+
+            foreach (var cliente in Cliente.Todos())
+            {
+                Console.WriteLine("=====================");
+                Console.WriteLine($"Id: {cliente.Id}");
+                Console.WriteLine($"Nome: {cliente.Nome}");
+
+            }
+
+             Console.WriteLine("=========[LENDO DO CSV]============");   
+             foreach (var cliente in Cliente.Todos(Tipo.CSV))
+            {
+                Console.WriteLine("=====================");
+                Console.WriteLine($"Id: {cliente.Id}");
+                Console.WriteLine($"Nome: {cliente.Nome}");
+
+            }
         }
     }
 }
